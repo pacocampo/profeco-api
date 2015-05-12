622d4446f638e409fe84ecbf52eae5ca0390849c
hydrosIII <galprasmarco@gmail.com>
2015-05-11 21:20:29 -0500

    adding super app in django

--- super/admin.py
+++ super/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Super)
+admin.site.register(models.Local)
+admin.site.register(models.Precios)
